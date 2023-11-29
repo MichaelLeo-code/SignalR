2c06b7be73a5aae2d0ad5f6913e675f58759381a
MichaelLeo-code <michael.leo.dk@gmail.com>
2023-11-29 13:50:49 +0100

    initial

---
+++ WebApplication1/MyHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplication1;
+
+[Authorize]
+public class MyHub : Hub<IMyClient>
+{
+    public override async Task OnConnectedAsync()
+    {
+        Clients.Client(Context.ConnectionId).Receive($"Hello, {Context.User?.Identity?.Name}");
+        await base.OnConnectedAsync();
+    }
+}
+
+public interface IMyClient
+{
+    Task Receive(string message);
+}
